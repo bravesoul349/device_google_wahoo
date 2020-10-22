8b0d7df5bf3cc98958dafc868e93b02f3527a7f9
Christian Oder <myself5@carbonrom.org>
2020-10-23 00:00:00 +0100

    wahoo: Add interfaces to PRODUCT_SOONG_NAMESPACES
    This allows pixel-power-ext-V1-ndk_platform.so to be built.
    
    Change-Id: I30261b43bc079faad09da635c9651d62ea045a24

--- device.mk
+++ device.mk
@@ -34,6 +34,7 @@ PRODUCT_SOONG_NAMESPACES += \
     device/google/wahoo \
     vendor/google/camera \
     hardware/google/camera \
+    hardware/google/interfaces \
     hardware/google/pixel \
     hardware/qcom/msm8998
 
